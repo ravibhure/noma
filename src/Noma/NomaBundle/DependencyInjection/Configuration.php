d20f3a017d60e4f2391c73daec752cf54ea44958
Jochem Kossen <jochem@jkossen.nl>
2012-12-03 22:41:12 +0100

    Add licensing information using php annotations

--- src/Noma/NomaBundle/DependencyInjection/Configuration.php
+++ src/Noma/NomaBundle/DependencyInjection/Configuration.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * Noma
+ *
+ * LICENSE
+ *
+ * This source file is subject to the GPLv3 license that is bundled
+ * with this package in the file doc/GPLv3.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ * @copyright  Copyright (c) 2012 Jochem Kossen <jochem@jkossen.nl>
+ * @license    http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ */
 
 namespace Noma\NomaBundle\DependencyInjection;
 
