e59a83fcd72f44317d98746a879980ab5ee7052f
Jochem Kossen <jochem@jkossen.nl>
2012-12-08 18:29:49 +0100

    Different setup of namespacing, I think this is more readable

--- src/Noma/NomaBundle/Resources/public/js/noma.js
+++ src/Noma/NomaBundle/Resources/public/js/noma.js
@@ -1,63 +1,80 @@
-/**
- * Noma
- *
- * LICENSE
- *
- * This source file is subject to the GPLv3 license that is bundled
- * with this package in the file doc/GPLv3.txt.
- * It is also available through the world-wide-web at this URL:
- * http://www.gnu.org/licenses/gpl-3.0.txt
- *
- * @copyright  Copyright (c) 2012 Jochem Kossen <jochem@jkossen.nl>
- * @license    http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
- */
+//
+// Noma
+// LICENSE
+//
+// This source file is subject to the GPLv3 license that is bundled
+// with this package in the file doc/GPLv3.txt.
+// It is also available through the world-wide-web at this URL:
+// http://www.gnu.org/licenses/gpl-3.0.txt
+//
+// @copyright  Copyright (c) 2012 Jochem Kossen <jochem@jkossen.nl>
+// @license    http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+//
 
-var noma = noma || {};
+var NOMA = NOMA || {};
 
-(function() {
-    var _ns = noma;
+////////////////////////////////////////////////////////////////////////////
+// SETUP
+// Base NOMA functions
+////////////////////////////////////////////////////////////////////////////
+(function(ns) {
     var _cfg = {};
 
-    this.services = {};
-    this.utilities = {};
-
-    this.init = function(cfg) {
-        this._cfg = cfg;
+    var get = function(key) {
+        return _cfg[key];
     }
 
-    /****** UTILITIES **********************************************************
-     * Collection of utility functions to make it easier to do certain things
-     */
+    var set = function(key, value) {
+        _cfg[key] = value;
+    }
 
-    /**
-     * Simple wrapper around jQuery's .ajax function
-     */
-    this.utilities.api_get = function(api_call, data, fn_success) {
+    var init = function(cfg) {
+        _cfg = cfg;
+    }
+
+    ns.get = get;
+    ns.set = set;
+    ns.init = init;
+}(NOMA));
+
+////////////////////////////////////////////////////////////////////////////
+// UTILITIES
+// Collection of utility functions to make it easier to do certain things
+////////////////////////////////////////////////////////////////////////////
+NOMA.utilities = NOMA.utilities || {};
+(function(ns) {
+
+    // Simple wrapper around jQuery's .ajax function
+    var api_get = function(api_call, data, fn_success) {
         $.ajax({
-            url: _ns._cfg['base_url'] + 'api/' + api_call + '/',
+            url: ns.get('base_url') + 'api/' + api_call + '/',
             type: 'GET',
             dataType: 'json',
             data: data,
             success: fn_success,
             error: function(jqXHR, textStatus, errorThrown) {
-                alert(errorThrown + ': ' + _cfg['base_url'] + 'api/' + api_call + '/');
+                alert(errorThrown + ': ' + NOMA.get('base_url') + 'api/' + api_call + '/');
             }
         });
     }
 
-    /****** SERVICES ***********************************************************
-     * Functions for the 'Services' screen
-     */
+    ns.utilities.api_get = api_get;
 
-    /**
-     * Show a list of nodes linked to the given nodeprop
-     */
-    this.services.show_nodes = function(nodeprop_id) {
+}(NOMA));
+
+////////////////////////////////////////////////////////////////////////////
+// SERVICES
+// Functions for the 'Services' screen
+////////////////////////////////////////////////////////////////////////////
+NOMA.services = NOMA.services || {};
+(function(ns) {
+    // Show a list of nodes linked to the given nodeprop
+    var show_nodes = function(nodeprop_id) {
         var data =  {
             nodeprop: nodeprop_id
         };
 
-        _ns.utilities.api_get('json_get_nodes', data, function(response, textStatus, jqXHR) {
+        ns.utilities.api_get('json_get_nodes', data, function(response, textStatus, jqXHR) {
             $('#nodeprop_nodes_' + nodeprop_id).append(
                 '<ul class="nodeprop_nodes" id="nodeprop_nodeslist_' + nodeprop_id + '">'
             );
@@ -69,15 +86,13 @@ var noma = noma || {};
         });
     }
 
-    /**
-     * Refresh the servicelist
-     */
-    this.services.refresh = function() {
+    // Refresh the servicelist
+    var refresh = function() {
         var data = {
             nodepropdefname: 'service'
         };
 
-        _ns.utilities.api_get('json_get_nodeprops', data, function(response, textStatus, jqXHR) {
+        ns.utilities.api_get('json_get_nodeprops', data, function(response, textStatus, jqXHR) {
             $.each(response.nodeprops, function(index, value) {
                 $('#servicelist_body').append(
                     '<tr>'
@@ -90,16 +105,18 @@ var noma = noma || {};
         });
     }
 
-    /**
-     * Event handler for the servicelist: when the nr of nodes link is clicked, show the nodes
-     */
-    this.services.init = function() {
+
+    // Event handler for the servicelist: when the nr of nodes link is clicked, show the nodes
+    var init = function() {
         $('#servicelist_body').on('click', function(event) {
             if ($(event.target).is('a.service_link')) {
                 var id = event.target.id.replace('#service_link_', '');
-                _ns.services.show_nodes(id);
+                ns.services.show_nodes(id);
             }
         });
     }
-}).apply(noma);
 
+    ns.services.init = init;
+    ns.services.refresh = refresh;
+    ns.services.show_nodes = show_nodes;
+}(NOMA));
