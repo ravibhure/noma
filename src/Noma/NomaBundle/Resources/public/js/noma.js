73c74dd0e9587430ef2e8b5dd397143e21db5331
Jochem Kossen <jochem@jkossen.nl>
2012-12-09 17:28:12 +0100

    Implement imperfect functionality for adding / removing nodes to / from services

--- src/Noma/NomaBundle/Resources/public/js/noma.js
+++ src/Noma/NomaBundle/Resources/public/js/noma.js
@@ -69,22 +69,66 @@ NOMA.utilities = NOMA.utilities || {};
 ////////////////////////////////////////////////////////////////////////////
 NOMA.services = NOMA.services || {};
 (function(ns) {
-    // Show a list of nodes linked to the given nodeprop
-    var show_nodes = function(nodeprop_id, el_target) {
-        var selected_nodes = [];
+    var add_node = function(nodeprop_id, node_id) {
+        var data = {
+            'node': node_id,
+            'nodeprop': nodeprop_id
+        };
+
+        ns.utilities.api_get('json_node_add_nodeprop', data, function(response, textStatus, jqXHR) {
+            $('#nodeprop_nodeslist_' + nodeprop_id).append(
+                $('#nodeprop_nodeslist_available_' + nodeprop_id + ' option:selected')
+            );
+        });
+    }
+
+    var remove_node = function(nodeprop_id, node_id) {
+        var data = {
+            'node': node_id,
+            'nodeprop': nodeprop_id
+        };
+
+        ns.utilities.api_get('json_node_remove_nodeprop', data, function(response, textStatus, jqXHR) {
+            $('#nodeprop_nodeslist_available_' + nodeprop_id).append(
+                $('#nodeprop_nodeslist_' + nodeprop_id + ' option:selected')
+            );
+        });
+    }
+
+    var refresh_nodes = function(nodeprop_id, el_target) {
+        $(el_target).empty();
+
+        $(el_target).append(
+            '<div class="pull-left"><strong>selected nodes:</strong><br>'
+            + '<select class="nodeprop_nodes" id="nodeprop_nodeslist_' + nodeprop_id + '" size="10"></select></div>'
+            + '<div class="btn-group btn-group-vertical pull-left" style="margin-top:75px;padding: 10px;">'
+            + '<button class="btn btn-success" id="select_node_' + nodeprop_id + '"><i class="icon-hand-left"></i></button>'
+            + '<button class="btn btn-danger" id="deselect_node_' + nodeprop_id + '"><i class="icon-hand-right"></i></button>'
+            + '</div>'
+            + '<div class="pull-left"><strong>available nodes:</strong><br>'
+            + '<select class="nodeprop_nodes" id="nodeprop_nodeslist_available_' + nodeprop_id + '" size="10"></select>'
+            + '</div>'
+        );
+
+        // event handler: add selected node to service
+        $('#select_node_' + nodeprop_id).click(function() {
+            var node_id = $('#nodeprop_nodeslist_available_' + nodeprop_id + ' option:selected').val();
+            if (node_id != undefined) {
+                add_node(nodeprop_id, node_id);
+            }
+        });
+
+        // event handler: remove selected node from service
+        $('#deselect_node_' + nodeprop_id).click(function() {
+            var node_id = $('#nodeprop_nodeslist_' + nodeprop_id + ' option:selected').val();
+            if (node_id != undefined) {
+                remove_node(nodeprop_id, node_id);
+            }
+        });
+
         var data =  {
             nodeprop: nodeprop_id
         };
-
-        $(el_target).empty();
-        $(el_target).toggle();
-
-        $(el_target).append(
-            '<select class="nodeprop_nodes" id="nodeprop_nodeslist_available_' + nodeprop_id + '" size="10"></select> <->'
-        );
-        $(el_target).append(
-            '<select class="nodeprop_nodes" id="nodeprop_nodeslist_' + nodeprop_id + '" size="10"></select>'
-        );
 
         ns.utilities.api_get('json_get_nodes', data, function(response, textStatus, jqXHR) {
             $.each(response.nodes, function(index, value) {
@@ -94,7 +138,7 @@ NOMA.services = NOMA.services || {};
             });
         });
 
-        var data2 =  {
+        var data2 = {
             exclude_nodeprop: nodeprop_id
         };
 
@@ -105,7 +149,13 @@ NOMA.services = NOMA.services || {};
                 );
             });
         });
+    }
 
+    // Show a list of nodes linked to the given nodeprop
+    var show_nodes = function(nodeprop_id, el_target) {
+        var selected_nodes = [];
+        $(el_target).toggle();
+        refresh_nodes(nodeprop_id, el_target);
     }
 
     // Refresh the servicelist
@@ -129,7 +179,6 @@ NOMA.services = NOMA.services || {};
             });
         });
     }
-
 
     // Event handler for the servicelist: when the nr of nodes link is clicked, show the nodes
     var init = function() {
