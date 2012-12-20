b367c5ca2b9e48bd039b4f16517665545b730c2d
Jochem Kossen <jochem@jkossen.nl>
2012-12-20 18:00:43 +0100

    Add test for removeNodeProp

--- src/Noma/NomaBundle/Tests/Entity/NodeEntityTest.php
+++ src/Noma/NomaBundle/Tests/Entity/NodeEntityTest.php
@@ -72,4 +72,20 @@ class NodeEntityTest extends WebTestCase
         $expect->add($np);
         $this->assertEquals($result, $expect);
     }
+
+    public function testNodeRemoveNodeProp()
+    {
+        $n = new Node();
+        $np = new NodeProp();
+        $np2 = new NodeProp();
+        $n->addNodeProp($np);
+        $n->addNodeProp($np2);
+
+        $n->removeNodeProp($np2);
+
+        $result = $n->getNodeprops();
+        $expect = new ArrayCollection();
+        $expect->add($np);
+        $this->assertEquals($result, $expect);
+    }
 }
