9cca4b68812c7d859daa7b02e9ede854325520c3
Jochem Kossen <jochem@jkossen.nl>
2013-02-18 21:23:34 +0100

    Add Unit test base file

---
+++ src/Noma/NomaBundle/Tests/UnitTestCase.php
@@ -0,0 +1,12 @@
+<?php
+namespace Noma\NomaBundle\Tests;
+
+abstract class UnitTestCase extends \PHPUnit_Framework_TestCase {
+    public function setUp() {
+        parent::setUp();
+    }
+
+    public function tearDown() {
+        parent::tearDown();
+    }
+}
