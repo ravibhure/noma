c4cc226fbf167259e60b3822c1b8d621e92befe8
Jochem Kossen <jochem@jkossen.nl>
2013-03-12 22:12:20 +0100

    Add more tests

--- src/Noma/NomaBundle/Tests/Controller/ApiControllerTest.php
+++ src/Noma/NomaBundle/Tests/Controller/ApiControllerTest.php
@@ -24,4 +24,97 @@ class ApiControllerTest extends FunctionalTestCase
 
         $this->assertEquals($response1->getContent(), $response2->getContent());
     }
+
+    public function testGetNodes()
+    {
+        $client = static::createClient();
+        $data = array('exclude_nodeprop' => 1);
+        $url = '/api/get_nodes/';
+
+        $crawler = $client->request('GET', $url, $data);
+        $response = $client->getResponse();
+
+        $this->assertTrue($response->isOk());
+    }
+
+    public function testNodeAddNodeprop()
+    {
+        $client = static::createClient();
+        $data = array(
+            'node' => 1,
+            'nodeprop' => 2
+        );
+        $url = '/api/node_add_nodeprop/';
+
+        $crawler = $client->request('GET', $url, $data);
+        $response = $client->getResponse();
+
+        $this->assertTrue($response->isOk());
+    }
+
+    public function testNodeAddNotExistingNodeprop()
+    {
+        $client = static::createClient();
+        $data = array(
+            'node' => 1,
+            'nodeprop' => 999999999999999999
+        );
+        $url = '/api/node_add_nodeprop/';
+
+        $crawler = $client->request('GET', $url, $data);
+        $response = $client->getResponse();
+
+        $this->assertTrue($response->isNotFound());
+    }
+
+    public function testNodeRemoveNodeprop()
+    {
+        $client = static::createClient();
+        $data = array(
+            'node' => 1,
+            'nodeprop' => 1
+        );
+        $url = '/api/node_remove_nodeprop/';
+
+        $crawler = $client->request('GET', $url, $data);
+        $response = $client->getResponse();
+
+        $this->assertTrue($response->isOk());
+    }
+
+    /**
+     * Test removing a nodeprop which does exist but is not linked to the node
+     */
+    public function testNodeRemoveNotLinkedNodeprop()
+    {
+        $client = static::createClient();
+        $data = array(
+            'node' => 1,
+            'nodeprop' => 2
+        );
+        $url = '/api/node_remove_nodeprop/';
+
+        $crawler = $client->request('GET', $url, $data);
+        $response = $client->getResponse();
+
+        $this->assertTrue($response->isOk());
+    }
+
+    /**
+     * Test removing a nonexisting nodeprop from a node
+     */
+    public function testNodeRemoveNotExistingNodeprop()
+    {
+        $client = static::createClient();
+        $data = array(
+            'node' => 1,
+            'nodeprop' => 99999999999999
+        );
+        $url = '/api/node_remove_nodeprop/';
+
+        $crawler = $client->request('GET', $url, $data);
+        $response = $client->getResponse();
+
+        $this->assertTrue($response->isNotFound());
+    }
 }
